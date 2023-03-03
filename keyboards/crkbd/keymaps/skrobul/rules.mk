0cf6f85c02bae4e2c07c062c00b88ed40187ec7d
Marek Skrobacki <skrobul@skrobul.com>
2023-03-03 21:52:33 +0000

    skrobul: add first corne configuration

---
+++ keyboards/crkbd/keymaps/skrobul/rules.mk
@@ -0,0 +1,13 @@
+MOUSEKEY_ENABLE = no     # Mouse keys
+
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+
+VIA_ENABLE      = no    # Enable VIA
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+
+COMBO_ENABLE    = yes
+CONSOLE_ENABLE  = no
