8c4365f64710f03019523749c1f1eb42678f300a
Marek Skrobacki <skrobul@skrobul.com>
2023-05-24 11:11:40 +0100

    fix macros

--- keyboards/qwertykeys/qk65/hotswap/keymaps/skrobul/keymap.c
+++ keyboards/qwertykeys/qk65/hotswap/keymaps/skrobul/keymap.c
@@ -30,14 +30,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
         CW_TOGG, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
         SC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC, KC_UP,   MO(1),
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, MO(1),          KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_VOLU,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TMUXPREV, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_END,
-        KC_TRNS,          KC_TRNS, KC_TRNS, TMUXCOPY, TMUXPASTE, KC_TRNS, TMUXNEXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,          KC_SLEP, KC_TRNS, TMUXCOPY, TMUXPASTE, KC_TRNS, TMUXNEXT, TMUXPREV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                   KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
@@ -59,30 +59,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-bool process_user_record(uint16_t keycode, keyrecord_t *record) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case TMUXNEXT:
       if (record->event.pressed) {
         SEND_STRING(SS_LCTL("b") SS_DELAY(50) "n");
       }
-      return false;
+    break;
+
     case TMUXPREV:
       if (record->event.pressed) {
         SEND_STRING(SS_LCTL("b") SS_DELAY(50) "p");
       }
-      return false;
+      break;
 
     case TMUXCOPY:
       if (record->event.pressed) {
         SEND_STRING(SS_LCTL("b") SS_DELAY(50) "[");
       }
-      return false;
+      break;
 
     case TMUXPASTE:
       if (record->event.pressed) {
         SEND_STRING(SS_LCTL("b") SS_DELAY(50) "]");
       }
-      return false;
+      break;
   }
   return true;
 }
